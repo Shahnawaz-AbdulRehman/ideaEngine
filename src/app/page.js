adf84f1a93164f8e8fd250cc0b428ea0cb1fc674
zaurmurtaza63 <zaurmurtaza63@gmail.com>
2024-07-24 19:23:06 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <div className="text-white">
-        hello world
-      </div>
+    <main className=" bg-black">
+      
       <Header />
       <FirstSection />
       
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './style.css'
-
+import Image from 'next/image'
+import logo from '../../../public/assets/logo.png';
 
 export default function Header() {
   return (
-    <div className='textWhite'>Header</div>
+   <>
+   <header>
+    <div className='flex'>
+      <div className='logo mw-100'>
+        <Image src={logo} />
+      </div>
+      <div className='header-menu'>
+        <ul>
+        <li><a href="#">White-paper</a></li>
+        <li><a href="#">VISUAL NOVEL</a></li>
+        <li><a href="#">INITIAL DEMO</a></li>
+        </ul>
+      </div>
+    </div>
+   </header>
+   </> 
   )
 }
