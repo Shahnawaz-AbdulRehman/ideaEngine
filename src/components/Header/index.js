41a82ebab8b496e17ee4b438b793bba6a3ff8153
zaurmurtaza63 <zaurmurtaza63@gmail.com>
2024-07-25 16:35:04 +0500

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,20 +2,24 @@ import React from 'react'
 import './style.css'
 import Image from 'next/image'
 import logo from '../../../public/assets/logo.png';
+import profile from '../../../public/assets/profile.png';
+import menuicon from '../../../public/assets/menu-icon.png';
+
+
 
 export default function Header() {
   return (
    <>
-   <header>
-    <div className='flex'>
-      <div className='logo mw-100'>
+ <header className='header w-full px-3'>
+ <div className='flex justify-between items-center'>
+ <div className='logo md:w-40 lg:w-auto'>
         <Image src={logo} />
       </div>
-      <div className='header-menu'>
-        <ul>
-        <li><a href="#">White-paper</a></li>
-        <li><a href="#">VISUAL NOVEL</a></li>
-        <li><a href="#">INITIAL DEMO</a></li>
+      <div className='header-menu py-5 xl:px-16 xl:px-16 md:px-6 hidden sm:block'>
+        <ul className='flex text-white xl:gap-12 md:gap-4 lg:gap-6'>
+        <li><a href="#" className='fs-18x uppercase'>White-paper</a></li>
+        <li><a href="#" className='fs-18x uppercase'>VISUAL NOVEL</a></li>
+        <li><a href="#" className='fs-18x uppercase'>INITIAL DEMO</a></li>
         </ul>
       </div>
       <div className='headerbtn-box flex items-center lg:gap-3 md:gap-1'>
@@ -23,9 +27,19 @@ export default function Header() {
         <div className='header-btn py-2 lg:px-4 md:px-2 text-white'><button><a href="#" className='fs-18x'>Connect Wallet</a></button></div>
         <div className='right-line h-7 w-2'></div>
         <div className='profile-img'> 
-          <Image src={profile} />
-    </div>
+        <Image src={profile} /> 
+        </div>
+        <input type='checkbox' id='check'></input>
+        <label for="check" className='checkbtn'>
+        <div className='menu-icon'> 
+        <Image src={menuicon} /> 
+        </div>
+        </label>
+
+
     </div>
     </div>
    </header> 
-   </>)};
+</>
+)
+};
