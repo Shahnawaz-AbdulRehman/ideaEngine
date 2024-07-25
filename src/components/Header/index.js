dc0f58fb6e3d666742d58ee6c8b67ebcd2c6192f
zaurmurtaza63 <zaurmurtaza63@gmail.com>
2024-07-25 23:44:18 +0500

    done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -16,27 +16,27 @@ import footerline from '../../../public/assets/footer-top-line.png';
 export default function Header() {
   return (
    <>
- <header className='header w-full px-3'>
+ <header className='header w-full px-3 md:mt-2 sm:mt-2'>
  <div className='flex justify-between items-center'>
  <div className='logo md:w-40 lg:w-auto'>
         <Image src={logo} />
       </div>
-      <div className='header-menu py-5 xl:px-16 xl:px-16 md:px-6 hidden sm:block'>
-        <ul className='flex text-white xl:gap-12 md:gap-4 lg:gap-6'>
-        <li><a href="#" className='fs-18x uppercase'>White-paper</a></li>
-        <li><a href="#" className='fs-18x uppercase'>VISUAL NOVEL</a></li>
-        <li><a href="#" className='fs-18x uppercase'>INITIAL DEMO</a></li>
+      <div className='header-menu py-5 xl:px-16 xl:px-16 md:px-6 hidden sm:block sm:px-3'>
+        <ul className='flex text-white xl:gap-12 md:gap-3 lg:gap-6 gap-4'>
+        <li><a href="#" className='menu-links uppercase'>White-paper</a></li>
+        <li><a href="#" className='menu-links uppercase'>VISUAL NOVEL</a></li>
+        <li><a href="#" className='menu-links uppercase'>INITIAL DEMO</a></li>
         </ul>
       </div>
       <div className='headerbtn-box flex items-center lg:gap-3 md:gap-1'>
         <div className='left-line h-7 w-2 bg-black'></div>
         <div className='header-btn py-2 lg:px-4 md:px-2 text-white'><button><a href="#" className='fs-18x'>Connect Wallet</a></button></div>
         <div className='right-line h-7 w-2'></div>
-        <div className='profile-img'> 
+        <div className='profile-img hidden sm:block'> 
         <Image src={profile} /> 
         </div>
         <input type='checkbox' id='check'></input>
-        <label for="check" className='checkbtn block sm:hidden'>
+        <label for="check" className='checkbtn block sm:hidden pl-4'>
         <div className='menu-icon'> 
         <Image src={menuicon} /> 
         </div>
