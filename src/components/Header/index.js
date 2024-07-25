972f52628ed7d553f167c66ce2342bb075fee0ed
zaurmurtaza63 <zaurmurtaza63@gmail.com>
2024-07-25 19:23:08 +0500

    updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -34,7 +34,7 @@ export default function Header() {
         <Image src={profile} /> 
         </div>
         <input type='checkbox' id='check'></input>
-        <label for="check" className='checkbtn'>
+        <label for="check" className='checkbtn block sm:hidden'>
         <div className='menu-icon'> 
         <Image src={menuicon} /> 
         </div>
@@ -43,8 +43,27 @@ export default function Header() {
     </div>
    </header> 
 
-   <footer>
-    <div></div>
+   <footer className='w-full'>
+    <div className='footer-top-line w-full'>
+      <div className='flex items-center justify-between'>
+
+        <div>
+        <ul className='flex footer-list text-white xl:gap-12 md:gap-4 lg:gap-6'>
+        <li><a href="#" className='fs-18x uppercase'>White-paper</a></li>
+        <li><a href="#" className='fs-18x uppercase'>VISUAL NOVEL</a></li>
+        <li><a href="#" className='fs-18x uppercase'>INITIAL DEMO</a></li>
+        </ul>
+        </div>
+
+        <div className='flex'>
+        <a href="#" className='fs-18x uppercase text-white'>Follow us</a>
+        <a href="#" className='fs-18x uppercase text-white'></a>
+        </div>
+
+        <div><div className='footer-btn py-2 lg:px-4 md:px-2 text-white'><button><a href="#" className='fs-18x'>Connect Wallet</a></button></div>
+        </div>
+      </div>
+    </div>
    </footer>
 </>
 )
